1330dd117d0e5a5ca195e8dfc0ad3fd9cb49d25e
lillie fitz <91137167+Winds-of-luck@users.noreply.github.com>
2025-12-18 11:09:57 +0000

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,31 +1,32 @@
 input.onButtonPressed(Button.A, function () {
     basic.showNumber(pins.analogReadPin(AnalogReadWritePin.P1))
 })
-input.onButtonPressed(Button.AB, function () {
-    fire_risk = temp * 20 / light2
-    if (fire_risk <= 2) {
-        basic.showString("there is a fire risk value of " + fire_risk + " low chance of fire")
-    } else if (fire_risk >= 2.1 && fire_risk <= 4) {
-        basic.showString("there is a fire risk value of " + fire_risk + " low chance of fire")
-    } else if (fire_risk >= 4.1 && fire_risk <= 6) {
-        basic.showString("there is a fire risk value of " + fire_risk + " low chance of fire")
-    } else if (fire_risk >= 6.1 && fire_risk <= 8) {
-        basic.showString("there is a fire risk value of " + fire_risk + " low chance of fire")
-    } else {
-        basic.showString("error in data")
-    }
-})
 input.onButtonPressed(Button.B, function () {
     basic.showNumber(input.temperature())
 })
-let light2 = 0
 let temp = 0
-let fire_risk = 0
+let light2 = 0
+let brightness = ""
 basic.showIcon(IconNames.Heart)
 serial.redirectToUSB()
 basic.forever(function () {
-    serial.writeLine("" + pins.analogReadPin(AnalogReadWritePin.P1) + "," + ("" + input.temperature()))
+    serial.writeLine("" + pins.analogReadPin(AnalogReadWritePin.P1) + "," + input.temperature() + "," + brightness)
+    basic.pause(2000)
+})
+basic.forever(function () {
     light2 = pins.analogReadPin(AnalogReadWritePin.P1)
     temp = input.temperature()
-    basic.pause(2000)
+    if (light2 >= 1000) {
+        brightness = "pitch black"
+    } else if (light2 >= 800 && light2 <= 1000) {
+        brightness = "very dark"
+    } else if (light2 >= 600 && light2 <= 800) {
+        brightness = "slightly dark"
+    } else if (light2 >= 400 && light2 <= 600) {
+        brightness = "normal light"
+    } else if (light2 >= 0 && light2 <= 400) {
+        brightness = "bright"
+    } else {
+        brightness = "bad data"
+    }
 })
